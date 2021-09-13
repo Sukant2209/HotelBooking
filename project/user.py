491f557fff8e88ea7eed675cab68192a67726cae
sukant2209 <sukant.sk22@gmail.com>
2021-09-13 17:28:52 +0530

    Added templates, static folder in the project

--- project/user.py
+++ project/user.py
@@ -1,8 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint , render_template
 
 user_blueprint = Blueprint("user_blueprint",__name__)
 
 
 @user_blueprint.route("/")
 def index():
-    return "hello world"
+    return render_template("register.html")
+
+
+@user_blueprint.route("/login")
+def login():
+    return render_template("login.html")
